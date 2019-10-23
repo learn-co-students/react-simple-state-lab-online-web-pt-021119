b9890f16c998ad75e9cfe7e1ea343de3bd8231d8
cyantis <cyantis@gmail.com>
2019-10-23 13:34:10 -0600

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -4,19 +4,19 @@ export default class Cell extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      color: this.props.value;
+      color: this.props.value
     }
   }
 
   changeColor = () => {
     this.setState({
-      color: '#333';
+      color: '#333'
     })
   }
 
   render(){
     return(
-      <div onClick={this.changeColor} className="cell" style={{backgroundColor: {this.state.color}}}>
+      <div onClick={this.changeColor} className="cell" style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
